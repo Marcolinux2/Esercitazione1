feb91dc36339a0ee04a9722bda26014a1d80fe36
Marcolinux2 <marco_tripolini@hotmail.com>
2023-07-14 15:40:43 +0200

    -> La vispa Teresa

--- Prova002/src/it/esperimento/Esperimento1.java
+++ Prova002/src/it/esperimento/Esperimento1.java
@@ -9,5 +9,10 @@ public class Esperimento1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Esperimento 1");
+		// La vispa Teresa
+		// avea tra l'erbetta
+		// in volo sorpresa
+		// gentil farfalletta!
+		
 	}
 }
