a6834af6862d22643926ecd48e7af96c6a1adb74
Marcolinux2 <marco_tripolini@hotmail.com>
2023-07-14 15:37:08 +0200

    Io sono un esempio di commit!

---
+++ Prova002/src/it/esperimento/Esperimento1.java
@@ -0,0 +1,13 @@
+package it.esperimento;
+
+public class Esperimento1 {
+
+	public Esperimento1() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Esperimento 1");
+	}
+}
